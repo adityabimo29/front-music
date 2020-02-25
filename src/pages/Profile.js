692d6b04e0b77dc8ce26241eeb417e600fb60cb1
Frans Jesky <fransjesky@hotmail.com>
2020-02-25 15:55:16 +0700

    patch 0.0.8 - completed comment section

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../assets/css/Profile.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import Picture from '../assets/images/01.jpg';
 
 export default class Profile extends Component {
@@ -10,7 +10,7 @@ export default class Profile extends Component {
         <Container>
           <Row>
             <Col xl={{ span: 4 }} className='ProfilePictureContainer'>
-              <img src={Picture} alt='Profile Picture' />
+              <img src={Picture} alt='Profile' />
             </Col>
             <Col xl className='Profile'>
               <h1>John Snow</h1>
@@ -51,46 +51,46 @@ export default class Profile extends Component {
             </Col>
           </Row>
           <Row>
-            <Col className='MediaContainer'>
+            <Col xl className='MediaContainer'>
               <h1>Media</h1>
               <Row>
                 <Col>
                   <iframe
-                    width='420'
+                    width='100%'
                     height='315'
-                    src={`https://www.youtube.com/embed/QvsQ9hYKq7c`}
+                    src={`https://www.youtube.com/embed/vxkl0kKXnHY`}
                   ></iframe>
                 </Col>
               </Row>
             </Col>
             <Col className='CommentContainer'>
-              <h1>Comment</h1>
+              <h1>Comments</h1>
               <Row>
                 <Col className='CommentSection'>
+                  <h5>Post by: Jesky</h5>
                   <p>
-                    <h5>Post by: Jesky</h5>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Placeat asperiores totam enim inventore incidunt, quod
                     necessitatibus provident pariatur molestiae consequatur,
                     unde nam obcaecati odit reiciendis hic aspernatur tempore.
                     Molestiae, cum.
                   </p>
+                  <h5>Post by: Aditya</h5>
                   <p>
-                    <h5>Post by: Aditya</h5>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Similique quis consectetur libero in, fugit sit delectus eos
                     veritatis unde veniam, amet exercitationem non rem provident
                     adipisci sed temporibus mollitia molestiae.
                   </p>
+                  <h5>Post by: Arya Stark</h5>
                   <p>
-                    <h5>Post by: Arya Stark</h5>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Iusto magni, modi quos, illum atque nihil praesentium
                     quibusdam hic, sunt consectetur tempore consequuntur eum.
                     Voluptatibus consectetur ut, ipsa beatae maxime quam.
                   </p>
+                  <h5>Post by: Mariah Carrey</h5>
                   <p>
-                    <h5>Post by: Mariah Carrey</h5>
                     Jesky Lorem ipsum dolor sit amet, consectetur adipisicing
                     elit. Consectetur harum laboriosam voluptas reiciendis ad
                     rem, aut voluptatem eius, aliquam earum vel omnis laborum
@@ -98,6 +98,27 @@ export default class Profile extends Component {
                   </p>
                 </Col>
               </Row>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xl>
+              <Form>
+                <Form.Group controlId='exampleForm.ControlTextarea1'>
+                  <Form.Label className='PostComment'>
+                    <h1>Post Comment</h1>
+                    <p>
+                      Once you post a comment, you will never be able to update
+                      or delete it. Please be careful with whatever you are
+                      going to comment.
+                      <br />
+                      Comments are visible to all visitor on this profile page.
+                    </p>
+                  </Form.Label>
+                  <Form.Control as='textarea' rows='5' />
+                  <Button className='PostButton'>Submit Post</Button>
+                </Form.Group>
+              </Form>
             </Col>
           </Row>
         </Container>
