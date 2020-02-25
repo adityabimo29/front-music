200e9a1ea141bdb41c1e8a841c38967b68c2dc4e
Frans Jesky <fransjesky@hotmail.com>
2020-02-25 12:02:01 +0700

    patch 0.0.5 - updated overflow effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
 import '../App.css';
-import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div className='myPage'>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import '../assets/css/Main.css';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import { Swiper, Navigation, Pagination } from 'swiper/js/swiper.esm';
-import ReactIdSwiperCustom from 'react-id-swiper/lib/ReactIdSwiper.custom';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import Swiper from 'react-id-swiper';
 // import Logo from '../assets/images/logo.png';
 import pic1 from '../assets/images/01.jpg';
 import pic2 from '../assets/images/02.jpg';
@@ -11,66 +10,65 @@ import pic3 from '../assets/images/03.jpg';
 export default class Main extends Component {
   render() {
     const params = {
-      // Provide Swiper class as props
-      Swiper,
-      // Add modules you need
-      modules: [Navigation, Pagination],
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true
+      },
       pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
+        el: '.swiper-pagination'
       },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
-      },
-      spaceBetween: 30
+      }
     };
+
     return (
       <div>
         <Container className='MainContainer'>
           <Row>
-            <ReactIdSwiperCustom {...params}>
-              <Col xl>
-                <Card className='Cards'>
-                  <Card.Img variant='top' src={pic1} />
-                  <Card.Body>
-                    <Card.Title>John Snow</Card.Title>
-                    <Card.Text>
-                      Rock
-                      <br />
-                      Guitarist
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col xl>
-                <Card className='Cards'>
-                  <Card.Img variant='top' src={pic2} />
-                  <Card.Body>
-                    <Card.Title>Ygritte</Card.Title>
-                    <Card.Text>
-                      Jazz
-                      <br />
-                      Singer
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col xl>
-                <Card className='Cards'>
-                  <Card.Img variant='top' src={pic3} />
-                  <Card.Body>
-                    <Card.Title>Arya Stark</Card.Title>
-                    <Card.Text>
-                      Rock
-                      <br />
-                      Bassist
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </ReactIdSwiperCustom>
+            <Swiper {...params}>
+              <Card className='Cards'>
+                <Card.Img variant='top' src={pic1} />
+                <Card.Body>
+                  <Card.Title>John Snow</Card.Title>
+                  <Card.Text>
+                    Rock
+                    <br />
+                    Guitarist
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card className='Cards'>
+                <Card.Img variant='top' src={pic2} />
+                <Card.Body>
+                  <Card.Title>Ygritte</Card.Title>
+                  <Card.Text>
+                    Jazz
+                    <br />
+                    Singer
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card className='Cards'>
+                <Card.Img variant='top' src={pic3} />
+                <Card.Body>
+                  <Card.Title>Arya Stark</Card.Title>
+                  <Card.Text>
+                    Rock
+                    <br />
+                    Bassist
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Swiper>
           </Row>
           <Row>
             <Col className='ButtonContainer'>
