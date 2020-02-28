a01f02d7525455ff646cf50b8f94b3cd66132116
Frans Jesky <fransjesky@hotmail.com>
2020-02-28 19:13:05 +0700

    optimized footer codes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,31 @@
 import React from 'react';
 import '../assets/css/Footer.css';
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Form,
-  FormGroup,
-  Input
-} from 'reactstrap';
+import { Container, Row, Col, Button, Form, FormGroup } from 'react-bootstrap';
 
 function MyFooter() {
   return (
     <div>
-      <Container className='Footer'>
+      <Container fluid className='Footer'>
         <Row>
-          <Col xl className='OfficeHours'>
-            <p className='OfficeHeader'>OFFICE HOURS</p>
-            <p>Weekday : 08.00 AM - 08.00 PM</p>
-            <p>Weekend : 10.00 AM - 05.00 PM</p>
-          </Col>
-
           <Col xl>
             <p className='TouchHeader'>GET IN TOUCH</p>
             <p>
               Subscribe to our newsletter to receive weekly news, updates,
               special offers, and exclusive discounts.
             </p>
-            <Row>
-              <Col xl>
-                <Form>
-                  <FormGroup>
-                    <Input
-                      type='email'
-                      name='email'
-                      id='exampleEmail'
-                      placeholder='please put your email'
-                    />
-                  </FormGroup>
-                </Form>
-              </Col>
-              <Col xl>
-                <Button>Subscribe Now</Button>
-              </Col>
-            </Row>
+            <Form className='FooterForm'>
+              <FormGroup>
+                <input
+                  type='email'
+                  name='email'
+                  placeholder='email@address.com'
+                  className='InputEmail'
+                />
+                <Button variant='outline-info' className='SubscribeButton'>
+                  Subscribe Now
+                </Button>
+              </FormGroup>
+            </Form>
           </Col>
         </Row>
       </Container>
