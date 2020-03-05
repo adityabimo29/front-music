5eb450841d6e8ce092cb0fa64cc8e3a376576559
Frans Jesky <fransjesky@hotmail.com>
2020-03-05 21:51:46 +0700

    updated team section

--- src/components/About/Team.js
+++ src/components/About/Team.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import '../../assets/css/Team.css';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import Avatar from '../../assets/images/github.png';
+import Avatar1 from '../../assets/images/teammember1.jpeg';
+import Avatar2 from '../../assets/images/teammember2.jpeg';
+import Avatar3 from '../../assets/images/teammember3.jpeg';
+import Avatar4 from '../../assets/images/teammember4.jpeg';
 
 export default class Team extends Component {
   render() {
@@ -16,14 +19,14 @@ export default class Team extends Component {
           <Row>
             <Col xl className='CardContainer'>
               <Card bg='dark' className='TeamCard'>
-                <Card.Img variant='top' src={Avatar} />
+                <Card.Img variant='top' src={Avatar1} />
                 <Card.Body>
                   <Card.Title className='CardTitle'>
                     <p>Aditya Bimo</p>
                   </Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    <p>Full Stack Dev</p>
+                    <p>Our team leader who handle the Front and Back End.</p>
                   </Card.Text>
                   <Button variant='outline-info'>View Github</Button>
                 </Card.Body>
@@ -31,14 +34,14 @@ export default class Team extends Component {
             </Col>
             <Col xl className='CardContainer'>
               <Card bg='dark' className='TeamCard'>
-                <Card.Img variant='top' src={Avatar} />
+                <Card.Img variant='top' src={Avatar2} />
                 <Card.Body>
                   <Card.Title className='CardTitle'>
                     <p>Frans Jesky</p>
                   </Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    <p>Front End Dev</p>A passionate Front End dev who focussed
+                    on UI/UX designs.
                   </Card.Text>
                   <Button variant='outline-info'>View Github</Button>
                 </Card.Body>
@@ -46,14 +49,14 @@ export default class Team extends Component {
             </Col>
             <Col xl className='CardContainer'>
               <Card bg='dark' className='TeamCard'>
-                <Card.Img variant='top' src={Avatar} />
+                <Card.Img variant='top' src={Avatar4} />
                 <Card.Body>
                   <Card.Title className='CardTitle'>
                     <p>Indra Surya</p>
                   </Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    <p>Front End Dev</p>
+                    Our team senior with most experience as Front End dev
                   </Card.Text>
                   <Button variant='outline-info'>View Github</Button>
                 </Card.Body>
@@ -61,14 +64,15 @@ export default class Team extends Component {
             </Col>
             <Col xl className='CardContainer'>
               <Card bg='dark' className='TeamCard'>
-                <Card.Img variant='top' src={Avatar} />
+                <Card.Img variant='top' src={Avatar3} />
                 <Card.Body>
                   <Card.Title className='CardTitle'>
                     <p>Elinda</p>
                   </Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    <p>Front End Dev</p>
+                    The only girl in our team with high dedication on
+                    programmnig
                   </Card.Text>
                   <Button variant='outline-info'>View Github</Button>
                 </Card.Body>
